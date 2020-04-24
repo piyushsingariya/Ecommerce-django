9304f1fa1b0bd7be7cc1b878a11d6f541267977b
Nectar <piyushsingariya@gmail.com>
2020-04-24 11:46:44 +0530

    Styling Order Admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 from .models import Item, OrderItem, Order, Coupon
 
+
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['user', 'ordered']
+    list_display = ['user',
+                    'ordered',
+                    'being_delivered',
+                    'received',
+                    'refund_requested',
+                    'refund_granted',
+                    # 'payment',
+                    'billing_address',
+                    'coupon']
+    list_display_links = ['user',
+                          # 'payment',
+                          'billing_address',
+                          'coupon']
+    list_filter = ['ordered',
+                   'being_delivered',
+                   'received',
+                   'refund_requested',
+                   'refund_granted']
+
 
 admin.site.register(Item)
 admin.site.register(Order, OrderAdmin)
--- core/models.py
+++ core/models.py
@@ -67,19 +67,30 @@ class OrderItem(models.Model):
 """All ordered Items together like a shopping cart"""
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    ref_code = models.CharField(max_length=30, default=0)
     items = models.ManyToManyField(OrderItem)
     start_date = models.DateTimeField(auto_now_add=True)
     ordered_date = models.DateTimeField()
     ordered = models.BooleanField(default=False)
     billing_address = models.ForeignKey('BillingAddress', on_delete=models.SET_NULL, blank=True, null=True)
     coupon = models.ForeignKey('Coupon', on_delete=models.SET_NULL, blank=True, null=True)
+    # payment = models.ForeignKey('Payment', on_delete=models.SET_NULL, blank=True, null=True)
+
+
+    being_delivered = models.BooleanField(default=False)
+    received = models.BooleanField(default=False)
+    refund_requested = models.BooleanField(default=False)
+    refund_granted = models.BooleanField(default=False)
+
+
     def __str__(self):
         return self.user.username
     def get_final_price(self):
         total = 0
         for order_item in self.items.all():
             total += order_item.get_total_price()
-        total -= self.coupon.amount
+        if self.coupon:
+            total -= self.coupon.amount
         return total
 
 
